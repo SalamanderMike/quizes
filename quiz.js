1685f1690f2b363b5a5fa16edfc63ba4c42763ae
salamandermike <salamandermike@gmail.com>
2014-09-15 10:06:33 -0700

    complete

--- quiz.js
+++ quiz.js
@@ -1,11 +1,7 @@
-#Javascript Quiz - Sept 15
+// 1. Make a `Person` constructor with attributes:
+// `name:string`, `height:string`, `age:number`, `sleeping:boolean`.
 
 
-##Questions
-
-1. Make a `Person` constructor with attributes: `name:string`, `height:string`, `age:number`, `sleeping:boolean`.
-
-```
 function Person (name, height, age, sleeping){
   this.name = name;
   this.height = height;
@@ -13,11 +9,11 @@ function Person (name, height, age, sleeping){
   this.sleeping = true;
 }
 
-```
 
-2. Add prototype methods to `person`: `eat`, `sleep`, and `wakeUp`. (The `sleep` and `wakeUp` methods should toggle `sleeping` to `true/false`, and `eat` should return an eating noise.)
+// 2. Add prototype methods to `person`: `eat`, `sleep`, and `wakeUp`.
+// (The `sleep` and `wakeUp` methods should toggle `sleeping` to `true/false`,
+// and `eat` should return an eating noise.)
 
-```
 Person.prototype.eat = function(){
   console.log("crunch... CRUNCH!");
 };
@@ -30,20 +26,16 @@ Person.prototype.wakeUp = function(){
   this.sleeping = false;
 };
 
-```
+// 3. Make a `Student` prototype that inherits from `person` and has the
+// additional attribute of `studying:boolean`.
 
-3. Make a `Student` prototype that inherits from `person` and has the additional attribute of `studying:boolean`.
-
-```
 Person.prototype.student = function(studying){
   this.studying = true;
 };
 
-```
 
-4. Add methods to `Student` called `study`, and `stopStudy` to toggle `studying`
+// 4. Add methods to `Student` called `study`, and `stopStudy` to toggle `studying`
 
-```
 Person.prototype.student = function(studying){
   this.studying = true;
 
@@ -60,11 +52,11 @@ Person.prototype.student = function(studying){
   };
 };
 
-```
 
-5. Override the `sleep` method on `student` to only run `sleep` if `studying` is `false`.
 
-```
+// 5. Override the `sleep` method on `student` to only run `sleep`
+// if `studying` is `false`.
+
 Person.prototype.sleep = function(studying){
   if (studying === false){
     this.sleep = true;
@@ -73,4 +65,7 @@ Person.prototype.sleep = function(studying){
   }
 };
 
-```
+
+
+
+
